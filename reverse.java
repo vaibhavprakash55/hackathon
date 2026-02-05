2de028c969ac4f98e4f4be5316b5a31af3bf6854
harshtiwari0001 <ht311453@gmail.com>
2026-02-06 00:01:05 +0530

    add problem

---
+++ reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class reverse{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number");
+        int n = sc.nextInt();
+        int revNum = 0;
+        while(n>0){
+            int ld = n%10;
+            revNum = (revNum*10) + ld;
+            n = n/10;
+        }
+        System.out.println("Reverse = " + revNum);
+    }
+}
