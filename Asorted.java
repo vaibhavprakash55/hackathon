3bc8654996ff799be7e6db65f3af7c1680f86a88
harshtiwari0001 <ht311453@gmail.com>
2026-02-06 00:06:40 +0530

    add problem

---
+++ Asorted.java
@@ -0,0 +1,12 @@
+public class ASorted{
+    public static void main(String args[]){
+        int arr[] = {1,3,5,6,7};
+        int n = arr.length;
+        for(int i =1; i<n; i++){
+            if(arr[i] >= arr[i-1]){
+                System.out.println("Sorted");
+                }
+
+            }
+        }
+    }
